7ec9f6078917ca01a481beaedd20d0f29e45349e
hrudalu <geward@seznam.cz>
2015-03-11 23:30:02 +0100

    Přidána třída ProjectData
    Přidána třída ProjectData. Tuhle třídu používejte v GUI pro přístup k
    třídám InterlacingData a LineData.

---
+++ Interlacer/Interlacer/ProjectData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interlacer
+{
+    class ProjectData
+    {
+        private InterlacingData interlacingData = new InterlacingData();
+        private LineData lineData = new LineData();
+
+        public InterlacingData GetInterlacingData()
+        {
+            return interlacingData;
+        }
+
+        public LineData GetLineData()
+        {
+            return lineData;
+        }
+    }
+}
